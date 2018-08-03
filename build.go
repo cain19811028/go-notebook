13e8794fbd52a94bf7b3cefe7a522e68367cef54
cain19811028 <cain19811028@gmail.com>
2018-08-03 21:54:26 +0800

    get repo commit info

--- build.go
+++ build.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bufio"
+	// "container/list"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 )
 
 var md = "framework.md"
@@ -17,6 +21,52 @@ var table = `
 | Project Name | Stars | Forks | Last Commit |
 | ------------ | ----- | ----- | ----------- |
 `
+
+type Repo struct {
+    DefaultBranch string `json:"default_branch"`
+}
+
+type Committer struct {
+	LastCommitDate string `json:"date"`
+}
+
+type Commit struct {
+    Committer Committer
+}
+
+type Commits struct {
+    Commit Commit
+}
+
+func load(token string, title string, file string) {
+	// l := list.New()
+	f, _ := os.Open("list/" + file)
+	scanner := bufio.NewScanner(f)
+    for scanner.Scan() {
+        url := scanner.Text()
+        if strings.HasPrefix(url, "https://github.com/") {
+        	path := api + url[19:] + "?access_token=" + token
+        	response, _ := http.Get(path)
+        	defer response.Body.Close()
+        	content, _ := ioutil.ReadAll(response.Body)
+        	var repo Repo
+        	json.Unmarshal([]byte(content), &repo)
+        	fmt.Println(repo.DefaultBranch)
+
+        	path = api + url[19:] + "/commits/" + repo.DefaultBranch + "?access_token=" + token
+        	response, _ = http.Get(path)
+        	defer response.Body.Close()
+        	content, _ = ioutil.ReadAll(response.Body)
+        	var commits Commits
+        	json.Unmarshal([]byte(content), &commits)
+        	fmt.Println(commits.Commit.Committer.LastCommitDate)
+        }
+    }
+}
+
+func build_info() {
+
+}
 
 func build_head() {
 	f, _ := os.Create("test.md")
@@ -36,8 +86,8 @@ func get_token() string {
 }
 
 func main() {
-	// token := get_token()
+	token := get_token()
     build_head()
+    load(token, "Web", "web_list.txt")
     build_tail()
-    fmt.Println("finish !")
 }
