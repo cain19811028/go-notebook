057a84b6ef01e3b2af0ee104cfaad4fd3e51651d
Hung-Chun Chen <cain19811028@gmail.com>
2020-10-11 14:08:19 +0800

    update framework.md

--- update.go
+++ update.go
@@ -27,17 +27,17 @@ var table = `
 var column = "| [%s](%s) | %s | %s | %s |\n"
 
 type Repo struct {
-	Name string `json:"name"`
-	HtmlUrl string `json:"html_url"`
-	StargazersCount int `json:"stargazers_count"`
-	ForksCount int `json:"forks_count"`
-    DefaultBranch string `json:"default_branch"`
-    LastCommitDate string
+	Name            string `json:"name"`
+	HtmlUrl         string `json:"html_url"`
+	StargazersCount int    `json:"stargazers_count"`
+	ForksCount      int    `json:"forks_count"`
+	DefaultBranch   string `json:"default_branch"`
+	LastCommitDate  string
 }
 
 type Commits struct {
-    Commit struct {
-	    Committer struct {
+	Commit struct {
+		Committer struct {
 			LastCommitDate string `json:"date"`
 		}
 	}
@@ -49,25 +49,26 @@ func load(token string, title string, file string) {
 	var commits Commits
 	f, _ := os.Open("list/" + file)
 	scanner := bufio.NewScanner(f)
-    for scanner.Scan() {
-        url := scanner.Text()
-        if strings.HasPrefix(url, "https://github.com/") {
-        	path := api + url[19:] + "?access_token=" + token
-        	content := getContent(path)
-        	json.Unmarshal(content, &repo)
+	for scanner.Scan() {
+		url := scanner.Text()
+		fmt.Println("url:" + url)
+		if strings.HasPrefix(url, "https://github.com/") {
+			path := api + url[19:] + "?access_token=" + token
+			content := getContent(path)
+			json.Unmarshal(content, &repo)
 
-        	path = api + url[19:] + "/commits/" + repo.DefaultBranch + "?access_token=" + token
-        	content = getContent(path)
-        	json.Unmarshal(content, &commits)
+			path = api + url[19:] + "/commits/" + repo.DefaultBranch + "?access_token=" + token
+			content = getContent(path)
+			json.Unmarshal(content, &commits)
 
-        	t, _ := time.Parse("2006-01-02T15:04:05Z", commits.Commit.Committer.LastCommitDate)
-        	repo.LastCommitDate = t.Format("2006-01-02 15:04:05")
-        	repos = append(repos, repo)
-        	fmt.Println(repo)
-        }
-    }
+			t, _ := time.Parse("2006-01-02T15:04:05Z", commits.Commit.Committer.LastCommitDate)
+			repo.LastCommitDate = t.Format("2006-01-02 15:04:05")
+			repos = append(repos, repo)
+			fmt.Println(repo)
+		}
+	}
 
-    sort.SliceStable(repos, func(i, j int) bool {
+	sort.SliceStable(repos, func(i, j int) bool {
 		return repos[i].StargazersCount > repos[j].StargazersCount
 	})
 
@@ -76,7 +77,7 @@ func load(token string, title string, file string) {
 
 func buildInfo(title string, repos []Repo) {
 	f, _ := os.OpenFile(md, os.O_APPEND|os.O_WRONLY, 0600)
-	f.WriteString(fmt.Sprintf(table, title))	
+	f.WriteString(fmt.Sprintf(table, title))
 	for _, repo := range repos {
 		result := fmt.Sprintf(column, repo.Name, repo.HtmlUrl, strconv.Itoa(repo.StargazersCount), strconv.Itoa(repo.ForksCount), repo.LastCommitDate)
 		f.WriteString(result)
@@ -102,6 +103,7 @@ func getToken() string {
 
 func getContent(path string) []byte {
 	response, _ := http.Get(path)
+	fmt.Println(response)
 	defer response.Body.Close()
 	content, _ := ioutil.ReadAll(response.Body)
 	return content
@@ -109,10 +111,10 @@ func getContent(path string) []byte {
 
 func main() {
 	token := getToken()
-    buildHead()
+	buildHead()
 	load(token, "Web", "web_list.txt")
-	load(token, "ORM", "orm_list.txt")
-    load(token, "Testing", "test_list.txt")
-    load(token, "IoT", "iot_list.txt")
-    buildTail()
+	// load(token, "ORM", "orm_list.txt")
+	// load(token, "Testing", "test_list.txt")
+	// load(token, "IoT", "iot_list.txt")
+	// buildTail()
 }
